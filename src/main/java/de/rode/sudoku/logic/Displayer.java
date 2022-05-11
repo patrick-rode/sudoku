9d974691be26757920a1e75ed1cc9b4d369e8b52
Patrick Rode <rode.patrick@gmail.com>
2022-05-11 19:42:24 +0200

    - das erzeugte HTML ist nicht mehr ganz so haesslich

--- src/main/java/de/rode/sudoku/logic/Displayer.java
+++ src/main/java/de/rode/sudoku/logic/Displayer.java
@@ -10,20 +10,54 @@ public class Displayer {
 
     public String display(Sudoku sudoku) {
 
-        final StringBuilder stringBuilder = new StringBuilder("<html><table border=\"1\"><h2>");
+        final StringBuilder stringBuilder = new StringBuilder("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "<style>\n" +
+                "table {\n" +
+                "  margin:1em auto;\n" +
+                "}\n" +
+                "td {\n" +
+                "  height:30px;\n" +
+                "  width:30px;\n" +
+                "  border:1px solid;\n" +
+                "  text-align:center;\n" +
+                "}\n" +
+                "td:first-child {\n" +
+                "  border-left:solid;\n" +
+                "}\n" +
+                "td:nth-child(3n) {\n" +
+                "  border-right:solid ;\n" +
+                "}\n" +
+                "tr:first-child {\n" +
+                "  border-top:solid;\n" +
+                "}\n" +
+                "tr:nth-child(3n) td {\n" +
+                "  border-bottom:solid ;\n" +
+                "}" +
+                "</style>\n" +
+                "</head>\n" +
+                "<body>" +
+
+                "<table id=\"sudoku\">\n" +
+                "\t<tbody>");
 
         for (int j=0; j<9; j++) {
             stringBuilder.append("<tr>");
             for (int i=0; i<9; i++) {
                 stringBuilder
-                        .append("<th>")
+                        .append("<td>")
                         .append(sudoku.getFields()[i][j])
-                        .append("</th>");
+                        .append("</td>");
             }
             stringBuilder.append("</tr>");
         }
         return stringBuilder
-                .append("</h2></table></html>")
+                .append("\t</tbody>\n" +
+                        "</table>\n" +
+                        "\n" +
+                        "</body>\n" +
+                        "</html>")
                 .toString();
     }
 
