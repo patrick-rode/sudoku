59944c25d7a3279d3b1773dcd98c8f19a17767e5
Patrick Rode <rode.patrick@gmail.com>
2022-05-11 17:29:28 +0200

    - server url angepasst

--- src/main/java/de/rode/sudoku/SudokuService.java
+++ src/main/java/de/rode/sudoku/SudokuService.java
@@ -6,14 +6,12 @@ import de.rode.sudoku.logic.Displayer;
 import de.rode.sudoku.logic.Parser;
 import de.rode.sudoku.logic.Solver;
 import de.rode.sudoku.logic.Validator;
-import io.vavr.control.Either;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +21,6 @@ import java.util.Optional;
 
 @Validated
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-@RequestMapping("/sudoku")
 @RestController
 @Slf4j
 public class SudokuService {
